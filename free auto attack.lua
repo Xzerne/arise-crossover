28fcb56d1ece6231d26763bd3ce4144238c2553d
Kujou Kurumi <167000432+Xzerne@users.noreply.github.com>
2025-03-31 17:49:39 +0700

    Create free auto attack.lua

---
+++ free auto attack.lua
@@ -0,0 +1,4 @@
+local cc = game:GetService("Players").LocalPlayer
+local ngu = cc:WaitForChild("leaderstats")
+local thieunang = ngu:WaitForChild("Passes")
+thieunang:SetAttribute("AutoAttack", true)
